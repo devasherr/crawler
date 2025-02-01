b34cc1e5f7310c54884739ffa1b487fdf2c577f0
devasherr <ashersam116@gmail.com>
2025-02-02 00:36:13 +0300

    refactored some stuff

--- main.go
+++ main.go
@@ -4,14 +4,19 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/spf13/pflag"
 )
 
-type searchInfo struct {
+type Config struct {
+	target      string
+	searchType  string
+	avoidFiles  map[string]bool
+	matchPrefix bool
 }
 
-func traverse(folderPath, target, searchType string, prefix bool, avoidFilesMap map[string]bool, prefixMatch *[]string) string {
+func traverse(folderPath string, config Config, prefixMatch *[]string) string {
 	directories, err := os.ReadDir(folderPath)
 	if err != nil {
 		return ""
@@ -22,36 +27,36 @@ func traverse(folderPath, target, searchType string, prefix bool, avoidFilesMap 
 		nextDir := filepath.Join(folderPath, dir.Name())
 
 		if dir.IsDir() {
-			if avoidFilesMap[dir.Name()] {
-				return result
+			if config.avoidFiles[dir.Name()] {
+				continue
 			}
-			if searchType == "any" || searchType == "folder" {
-				if dir.Name() == target {
+			if config.searchType == "any" || config.searchType == "folder" {
+				if dir.Name() == config.target {
 					return nextDir
-				} else if prefix && checkPrefix(target, dir.Name()) {
+				} else if config.matchPrefix && checkPrefix(dir.Name(), config.target) {
 					*prefixMatch = append(*prefixMatch, nextDir)
 				}
 			}
 
-			curRes := traverse(nextDir, target, searchType, prefix, avoidFilesMap, prefixMatch)
+			curRes := traverse(nextDir, config, prefixMatch)
 			if len(curRes) > 1 {
 				result = curRes
 			}
 		} else {
-			if avoidFilesMap[dir.Name()] {
+			if config.avoidFiles[dir.Name()] {
 				return result
 			}
 
-			if searchType == "any" || searchType == "file" {
-				if dir.Name() == target {
+			if config.searchType == "any" || config.searchType == "file" {
+				if dir.Name() == config.target {
 					return nextDir
-				} else if prefix && checkPrefix(target, dir.Name()) {
+				} else if config.matchPrefix && checkPrefix(dir.Name(), config.target) {
 					*prefixMatch = append(*prefixMatch, nextDir)
 				}
 			}
 
-			if dir.Name() == target && !avoidFilesMap[dir.Name()] {
-				if searchType == "any" || searchType == "file" {
+			if dir.Name() == config.target && !config.avoidFiles[dir.Name()] {
+				if config.searchType == "any" || config.searchType == "file" {
 					return nextDir
 				}
 			}
@@ -62,40 +67,35 @@ func traverse(folderPath, target, searchType string, prefix bool, avoidFilesMap 
 }
 
 func checkPrefix(word, target string) bool {
-	if len(word) > len(target) {
-		return false
-	}
-	for i := 0; i < len(word); i++ {
-		if word[i] != target[i] {
-			return false
-		}
-	}
-	return true
+	return strings.HasPrefix(word, target)
 }
 
-func main() {
-	var searchType string
+func parseFlag() Config {
+	c := Config{}
 	var avoidFiles []string
-	var matchPrefix bool
-	pflag.StringVarP(&searchType, "type", "t", "any", "operate on folder or file")
+	pflag.StringVarP(&c.searchType, "type", "t", "any", "operate on folder or file")
 	pflag.StringSliceVarP(&avoidFiles, "exclude", "x", []string{""}, "folder or file to exclude from the search")
-	pflag.BoolVarP(&matchPrefix, "prefix", "p", false, "return closest matching prefix if target not found")
+	pflag.BoolVarP(&c.matchPrefix, "prefix", "p", false, "return closest matching prefix if target not found")
 	pflag.Parse()
 
-	var avoidFilesMap = make(map[string]bool)
+	c.avoidFiles = make(map[string]bool)
 	for _, file := range avoidFiles {
-		avoidFilesMap[file] = true
+		c.avoidFiles[file] = true
 	}
 
 	if len(pflag.Args()) == 0 {
-		fmt.Println("no target provided")
-		return
+		panic("no target provided")
 	}
-
 	if len(pflag.Args()) > 1 {
-		fmt.Println("too many targets provided")
-		return
+		panic("too many targets provided")
 	}
+	c.target = pflag.Args()[0]
+
+	return c
+}
+
+func main() {
+	config := parseFlag()
 
 	folderPath, err := os.Getwd()
 	if err != nil {
@@ -103,11 +103,11 @@ func main() {
 	}
 
 	prefixMatch := []string{}
-	result := traverse(folderPath, pflag.Args()[0], searchType, matchPrefix, avoidFilesMap, &prefixMatch)
+	result := traverse(folderPath, config, &prefixMatch)
 	if result == "" {
 		fmt.Println(">> No match found !!")
 
-		if matchPrefix {
+		if config.matchPrefix {
 			if len(prefixMatch) == 0 {
 				fmt.Println(">> >> No prefix found")
 			} else {
